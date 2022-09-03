c62d9cccc8070cd5d8f723f7cc4e6ed086e2d559
Ingo Ruhnke <grumbel@gmail.com>
2022-09-03 13:26:18 +0200

    Move ipflib into its own flake

--- flake.nix
+++ flake.nix
@@ -5,38 +5,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
 
-    ipflib-src.url = "http://www.softpres.org/_media/files:ipflib42_linux-x86_64.tar.gz?id=download&cache=cache";
-    ipflib-src.flake = false;
+    ipflib.url = "github:grumnix/ipflib";
+    ipflib.inputs.nixpkgs.follows = "nixpkgs";
+    ipflib.inputs.flake-utils.follows = "flake-utils";
 
     disk-utilities-src.url = "github:keirf/disk-utilities";
     disk-utilities-src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, ipflib-src, disk-utilities-src }:
+  outputs = { self, nixpkgs, flake-utils, ipflib, disk-utilities-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages = rec {
           default = disk-utilities;
-
-          ipflib = pkgs.stdenv.mkDerivation rec {
-            pname = "ipflib";
-            version = "4.2";
-
-            src = ipflib-src;
-
-            installPhase = ''
-              mkdir -p $out $out/lib
-              cp -vr include $out
-              cp -v libcapsimage.so.4.2 $out/lib
-              ln -s libcapsimage.so.4.2 $out/lib/libcapsimage.so.4
-            '';
-
-            postFixup = ''
-              patchelf --set-rpath ${pkgs.stdenv.cc.cc.lib}/lib $out/lib/libcapsimage.so.4.2
-            '';
-          };
 
           disk-utilities = pkgs.stdenv.mkDerivation rec {
             pname = "disk-utilities";
@@ -53,7 +36,7 @@
             installPhase = "caps=y make install PREFIX=$out";
 
             buildInputs = [
-              ipflib
+              ipflib.packages.${system}.default
             ];
           };
         };
