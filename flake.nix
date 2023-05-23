d568dd9d2990f8ebc6635cfbf6a8a010ce28c08e
Ingo Ruhnke <grumbel@gmail.com>
2023-05-23 20:30:46 +0200

    Update flake.nix to nixos-23.05 and disk-utilities-src

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A collection of utilities for ripping, dumping, analysing, and modifying disk images.";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     ipflib.url = "github:grumnix/ipflib";
