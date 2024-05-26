c40cb170cf6571dd1539464ceb2a2de2a3821d0a
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:54:48 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A collection of utilities for ripping, dumping, analysing, and modifying disk images.";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     ipflib.url = "github:grumnix/ipflib";
